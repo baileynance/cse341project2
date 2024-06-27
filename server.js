579abae1e18510e5e5dc55100791f4ea3fb1977f
baileynance <baieyj.nance@gmail.com>
2024-06-26 20:12:40 -0700

    project initalize

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
-const router = express.Router();
 
-const port = 3000;
- 
-app.get('/', (req, res) => {
-  res.send("Hello");
-});
- 
-app.listen(process.env.PORT || port, () => {
-  console.log('Web Server is listening at port ' + (process.env.PORT || port));
+const mongodb = require('./data/database');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers", 
+        "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    next();
+}) 
+app.use('/', require('./routes'));
+
+mongodb.initDB((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Database is listening and node app is running on port ${port}`);
+    });
+  }
 });
